487ed738feb4d3bd17bf402d3d56819fd9ad7037
Nate Sticco <nsticco@gmail.com>
2023-01-06 15:20:06 -0800

    get webui building

--- webui/config-overrides.js
+++ webui/config-overrides.js
@@ -2,6 +2,8 @@ module.exports = function override(config, env) {
   config.resolve.fallback = {
     crypto: require.resolve("crypto-browserify"),
     stream: require.resolve("stream-browserify"),
+    util: require.resolve("util/"),
+    buffer: require.resolve("buffer/"),
   };
 
   return config;
