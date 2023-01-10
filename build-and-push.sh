ffc524f9fc17d5d9010ad6659f97d90282a2c645
Nate Sticco <nsticco@gmail.com>
2023-01-10 11:23:52 -0800

    allow region specification

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,9 +1,10 @@
-docker build -t watchman-prd .
+docker build -t watchman-prd . --no-cache
 
 # Get repo URI from WATCHMAN_REPO in .env
 repo=$(grep WATCHMAN_REPO .env | cut -d '=' -f 2)
+region=$(grep AWS_REGION .env | cut -d '=' -f 2)
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $repo
+aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $repo
 
 docker tag watchman-prd:latest $repo/watchman-prd:latest
 
