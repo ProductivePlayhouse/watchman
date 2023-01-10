e2fda50bd39295afcddc69bcabf8cc0f53cfb675
Nate Sticco <nsticco@gmail.com>
2023-01-10 11:33:45 -0800

    use consistent env var names

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,11 +1,11 @@
 docker build -t watchman-prd . --no-cache
 
 # Get repo URI from WATCHMAN_REPO in .env
-repo=$(grep WATCHMAN_REPO .env | cut -d '=' -f 2)
-region=$(grep AWS_REGION .env | cut -d '=' -f 2)
+WATCHMAN_REPO=$(grep WATCHMAN_REPO .env | cut -d '=' -f 2)
+AWS_REGION=$(grep AWS_REGION .env | cut -d '=' -f 2)
 
-aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $repo
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $WATCHMAN_REPO
 
-docker tag watchman-prd:latest $repo/watchman-prd:latest
+docker tag watchman-prd:latest $WATCHMAN_REPO/watchman-prd:latest
 
-docker push $repo/watchman-prd:latest
+docker push $WATCHMAN_REPO/watchman-prd:latest
