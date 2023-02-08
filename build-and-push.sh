865bc4285b995c0ee6b6aba99b7db1fdbc1ff019
Nate Sticco <nsticco@gmail.com>
2023-02-07 16:49:46 -0800

    reorg commands for easier copy paste

--- build-and-push.sh
+++ build-and-push.sh
@@ -2,12 +2,11 @@
 # NOTE: .env file must already exist
 CONTAINER_REPO=$(grep CONTAINER_REPO .env | cut -d '=' -f 2)
 AWS_REGION=$(grep AWS_REGION .env | cut -d '=' -f 2)
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $CONTAINER_REPO
 
 docker system prune -f
 
 docker build -t watchman-prd . --no-cache --build-arg CONTAINER_REPO=$CONTAINER_REPO
-
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $CONTAINER_REPO
 
 docker tag watchman-prd:latest $CONTAINER_REPO/watchman-prd:latest
 
